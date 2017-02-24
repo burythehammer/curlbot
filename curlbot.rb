f4a2729b3ad0ed036278dc47564e1c0124349560
Matt Long <matt@burythehammer.com>
2017-02-24 14:36:31 +0000

    Fixed issues with rest responses, now gets back a list of results

--- curlbot.rb
+++ curlbot.rb
@@ -1,26 +1,46 @@
 require 'rest-client'
 
-base_url_list = ["profilebooks", "serpentstail", "profilebusiness"]
-
-
-def get_url(baseurl, forename, surname)
-  "https://#{baseurl}.com/#{forename}-#{surname}.html"
+def get_url(baseurl, name)
+  "http://#{baseurl}.com/#{name}.html"
 end
 
-def get_response_for_url
-    RestClient.get(url) { |response, request, result, &block|
-    case response.code
-    when 200
-        p "#{url} exists"
-    when 404
-        p "#{url} does not exist"
-    else
-        response.return!(request, result, &block)
+def url_response_code(url)
+  begin
+    response = RestClient::Request.execute(method: :head, url: url, verify_ssl: false)
+    return response.code
+  rescue RestClient::ExceptionWithResponse => err
+    return err.response.code
+  end
+end
+
+def get_url_codes(namelist, base_url_list)
+  url_codes = []
+
+  namelist.each do |name|
+    base_url_list.each do |base_url|
+      url = get_url(base_url, name)
+      code = url_response_code(url)
+      p "#{url} returned #{code}"
+      url_codes.push("#{url}: #{code}")
     end
-    }
+  end
+
+  url_codes
 end
 
-url = get_url('profilebooks','Matt', 'Long')
+def parse_names(filepath)
+  names = File.read(filepath).downcase.lines.map(&:chomp)
+  names.map do |name|
+    name.tr!(' ', '-') if name.include? ' '
+  end
+end
 
+def parse_base_urls(filepath)
+  File.read(filepath).downcase.lines.map(&:chomp)
+end
 
+namelist = parse_names('names.txt')
+base_url_list = parse_base_urls('base_urls.txt')
+urlcodes = get_url_codes(namelist, base_url_list)
 
+urlcodes.each {|r| p r}
